31a21ddb9dcf77c888e96997c98e624bd356b7b9
Dmitry Ponyatov <dponyatov@gmail.com>
2016-11-09 17:45:59 +0400

    * hello/button
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- hello.py
+++ hello.py
@@ -1,9 +1,10 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.button import Button
 
 class My(App):
     def build(self):
-        return Label(text='Hello world')
+        return Button(text='Hello world')
 
 My().run()
