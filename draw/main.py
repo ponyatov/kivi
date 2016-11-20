1b2b84dfe80a200f675d8ec100ef9c5fcd62cbd9
Dmitry Ponyatov <dponyatov@gmail.com>
2016-11-20 23:27:34 +0400

    + draw/ form
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- draw/main.py
+++ draw/main.py
@@ -1,7 +1,18 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.button import Button
 
+class Form(Widget):
+    b = Button()
+    def __init__(self,**args):
+        super(self.__class__,self).__init__(**args)
+        self.add_widget(self.b)
+    def on_touch_down(self,touch):
+        print touch
+#         if touch.pos[1]>self.btn.pos[1]:
+        self.b.pos = touch.pos
+ 
 class Draw(App):
     def build(self):
-        return Widget()
+        return Form()
 Draw().run()
