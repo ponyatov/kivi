ccaca97bf380b75f2c34d7ecb2b71743d764bcde
Dmitry Ponyatov <dponyatov@gmail.com>
2016-11-21 10:00:56 +0400

    * draw/ works
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- draw/main.py
+++ draw/main.py
@@ -1,16 +1,14 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
+from kivy.graphics import Line
 
 class Form(Widget):
-    b = Button()
-    def __init__(self,**args):
-        super(self.__class__,self).__init__(**args)
-        self.add_widget(self.b)
     def on_touch_down(self,touch):
-        print touch
-#         if touch.pos[1]>self.btn.pos[1]:
-        self.b.pos = touch.pos
+        with self.canvas:
+            touch.ud["line"] = Line(points=(touch.x,touch.y))
+    def on_touch_move(self, touch):
+        touch.ud["line"].points += (touch.x,touch.y)
  
 class Draw(App):
     def build(self):
